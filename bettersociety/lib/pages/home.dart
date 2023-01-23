953d6b09c5b45baf6f98b02bc3140538faef587e
kchakarov <kchakarov@appolica.com>
2023-01-23 15:23:45 +0200

    Added login, registration and forgot password logic with bacis design

--- bettersociety/lib/pages/home.dart
+++ bettersociety/lib/pages/home.dart
@@ -1,17 +1,1 @@
-import 'dart:html';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'dart:html';
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Text("Home");
-  }
-}
--- bettersociety/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bettersociety/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_functions
+import firebase_app_check
+import firebase_auth
+import firebase_core
+import firebase_database
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
 }
